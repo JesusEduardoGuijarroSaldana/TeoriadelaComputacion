7fbf45ce662cf4de161fd65dfaf998cb812549c3
JesusEduardoGuijarroSaldana <guijarro497@gmail.com>
2022-02-17 19:27:08 -0600

    Prueba

---
+++ TeoriadelaComputacion/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: guija
+ *
+ * Created on 17 de febrero de 2022, 07:13 PM
+ */
+
+#include <cstdlib>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Hola";
+    return 0;
+}
+
